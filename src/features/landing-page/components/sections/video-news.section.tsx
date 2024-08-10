8a4631c9bb0039a087b99e4df0a0522571d283ec
Nur Ahmadi <ahmadinur2202@gmail.com>
2024-08-10 22:48:48 +0800

    refactor: update docs youtube highlight

--- src/features/landing-page/components/sections/video-news.section.tsx
+++ src/features/landing-page/components/sections/video-news.section.tsx
@@ -1,30 +1,27 @@
 "use client";
 
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import Image from "next/image";
+import Link from "next/link";
 
 const videoItems = [
   {
-    title:
-      "Pembukaan Pameran Tunggal Herman Ong Journey To A Thousand Expressions oleh Harda Kiswaya",
-    date: "14 Juli 2024",
-    thumbnail: "/images/video-1.png",
-  },
-  {
-    title: "Ngiras : Menikmati Kelezatan Bakmi Pak Mo Tempel Bersama Pak Harda",
+    title: "Hantam Kiswaya Vol. 1 di Rakjat Coffee & Eatery",
     date: "11 Juli 2024",
-    thumbnail: "/images/video-2.png",
+    thumbnail: "/youtube/yt-1.jpg",
+    link: "https://youtu.be/BuaziHtB2MM?si=exu-2IhYDsPnq0Vr",
   },
   {
-    title:
-      "Pembukaan Pameran Tunggal Herman Ong Journey To A Thousand Expressions oleh Harda Kiswaya",
+    title: "Matur Nuwun Kepercayaan & Dukungannya",
     date: "14 Juli 2024",
-    thumbnail: "/images/video-1.png",
-  },
-  {
-    title: "Ngiras : Menikmati Kelezatan Bakmi Pak Mo Tempel Bersama Pak Harda",
-    date: "11 Juli 2024",
-    thumbnail: "/images/video-2.png",
+    thumbnail: "/youtube/yt-2.jpg",
+    link: "https://youtu.be/7KDnQbH4PBg?si=Dm2PXKFLzhvKeegG",
   },
 ];
 
@@ -48,25 +45,27 @@ const VideoNewsSection = () => {
                   key={index}
                   className="flex  flex-col items-center justify-center"
                 >
-                  <div className="relative">
-                    <Image
-                      src={video.thumbnail}
-                      alt={video.title}
-                      width={1000}
-                      height={1000}
-                      className="h-[145px] w-[260px] lg:w-[800px] lg:h-[450px] rounded-lg"
-                    />
-                    <Image
-                      className="h-[50px] w-[50px] absolute top-0 bottom-0 my-auto right-0 left-0 mx-auto"
-                      src="/icons/play-icon.svg"
-                      alt="play-button"
-                      width={50}
-                      height={50}
-                    />
-                  </div>
-                  <p className="max-w-[260px] lg:max-w-[800px] lg:text-3xl text-center mt-[10px] text-sm font-semibold text-neutral-900">
-                    {video.title}
-                  </p>
+                  <Link href={video.link} target="_blank">
+                    <div className="relative">
+                      <Image
+                        src={video.thumbnail}
+                        alt={video.title}
+                        width={1000}
+                        height={1000}
+                        className="h-[145px] w-[260px] lg:w-[800px] lg:h-[450px] rounded-lg"
+                      />
+                      <Image
+                        className="h-[50px] w-[50px] absolute top-0 bottom-0 my-auto right-0 left-0 mx-auto"
+                        src="/icons/play-icon.svg"
+                        alt="play-button"
+                        width={50}
+                        height={50}
+                      />
+                    </div>
+                    <p className="max-w-[260px] lg:max-w-[800px] lg:text-3xl text-center mt-[10px] text-sm font-semibold text-neutral-900">
+                      {video.title}
+                    </p>
+                  </Link>
                 </CarouselItem>
               ))}
             </CarouselContent>
